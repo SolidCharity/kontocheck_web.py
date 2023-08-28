2083a2fee42ddcb46c66a99b467a1c63fcf551d6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2023-08-28 08:31:39 +0200

    update lut file to 2023-08

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,4 @@
 wget https://sourceforge.net/projects/kontocheck/files/konto_check-de/6.15/blz.lut2f/download -O ~/pyenv/lib/python3.7/site-packages/kontocheck/data/blz.lut2
 head ~/pyenv/lib/python3.7/site-packages/kontocheck/data/blz.lut2 | grep "gueltig vom" > ~/kontocheck/src/version.txt
 head ~/pyenv/lib/python3.7/site-packages/kontocheck/data/blz.lut2
-./restart.sh
+~/bin/restart.sh
